31893fe98533d0b1d31052580d9f6a13917cc161
Markusha <LegoAnimation2022@gmail.com>
2025-02-27 16:26:03 +0000

    1

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+const puppeteer = require('puppeteer');
 
+const token = '7715514174:AAHcby70BM39h8B4V5YkGb_MaiK2CfT2_ik';
+const bot = new TelegramBot(token, {polling:true});
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, `Здравствуйте, ${msg.from.first_name}!\nМы предоставляем вам возможность пранка.\n`);
+});
+
+console.log('> Successful start');
