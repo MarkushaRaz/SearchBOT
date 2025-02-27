7b88ca3422661601f4ee2f2864561342e48f536f
Markusha <LegoAnimation2022@gmail.com>
2025-02-27 19:01:30 +0000

    2

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 const TelegramBot = require('node-telegram-bot-api');
 const puppeteer = require('puppeteer');
 
-const token = '7715514174:AAHcby70BM39h8B4V5YkGb_MaiK2CfT2_ik';
+const token = '7715514174:AAGk4TTEyFHRidw2KxkcYVbcgE9XiR9dHkE';
 const bot = new TelegramBot(token, {polling:true});
 
+let Data = new Map();
+
+let TimeData;
+
+function SaveData(userId, data) {
+    Data.set(userId, data)
+}
+
+function GetData(userId) {
+    return Data.get(userId);
+}
+
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, `Здравствуйте, ${msg.from.first_name}!\nМы предоставляем вам возможность пранка.\n`);
+    TimeData = GetData(msg.chat.id);
+
+    if (TimeData === `${msg.from.username}, ${msg.chat.id}`) {
+
+    }
+    SaveData(msg.chat.id, `${msg.from.username}, ${msg.chat.id}`);
+
+    bot.sendMessage(msg.chat.id, `Здравствуйте, ${msg.from.first_name}!\n\nМы предоставляем вам возможность поиска ответов для школы.`);
 });
+
+bot.onText(/\/account/, (msg) => {});
+
+bot.onText(/\/list/, () => {});
 
 console.log('> Successful start');
